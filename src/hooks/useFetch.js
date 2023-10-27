5d41acc16d1285a853836421d31fa7b7436f8868
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-10-27 20:27:15 +0100

    useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+
+const URL = 'https://jsonplaceholder.typicode.com/todos';
+
+const useFetch = (URL) => {
+
+    const [data, setData] = useState('')
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+       
+        fetch(URL)
+        .then((res) => {
+            if(!res.ok) {
+                throw new Error('failed to fetch the data')
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setData(data);
+            setError(null);
+            setLoading(false);
+        })
+        .catch(() => {
+            setData([]);
+            setError(error.message);
+            setLoading(false)
+        });
+
+   }, [])
+
+
+  return {
+    data, loading, error
+  }
+}
+
+export default useFetch
