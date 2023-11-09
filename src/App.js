32dea90f19917e532f830f2833b61c9413b3f2e9
Alexander Sibhatu <sibhatualexander@gmail.com>
2023-11-09 09:45:20 +0000

    useFetch hook used

--- src/App.js
+++ src/App.js
@@ -1,63 +1,48 @@
-import React, {useState} from 'react'
-import { v4 as uuidv4 } from 'uuid';
+// import React, {useState} from 'react'
+// import { v4 as uuidv4 } from 'uuid';
 
-import Countries from "./components/Countries";
-import NewCountry from './components/NewCountry';
+// import Countries from "./components/Countries";
+// import NewCountry from './components/NewCountry';
 
-import Title from './Title';
-import './App.css'
+// import Title from './Title';
+import './App.css';
+import useFetch from './hooks/useFetch';
+import Todos from './Todos';
 
 function App() {
-  const initialCountries = [ 
-    {
-      id: uuidv4(),
-      name: "Kenya",
-      population: "53,010,000",
-      capital: "Nairobi",
-    },
-    {
-      id: uuidv4(),
-      name: 'Ethiopia',
-      population: "140,000,000",
-      capital: "Addis Ababa",
-    },
-    {
-      id: uuidv4(),
-      name: "Eritrea",
-      population: "4,000,000",
-      capital: "Asmera",
-    },
-    {
-      id: uuidv4(),
-      name: "Sudan",
-      population: "45,660,000",
-      capital: "Kartum",
-    },
-    {
-      id: uuidv4(),
-      name: "Northern Ireland",
-      population: "1,885,000",
-      capital: "Belfast",
-    }
-  ]
 
-  const [countries, setCountries] = useState(initialCountries)
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  // const initialCountries = [
+  //   {
+  //     id: uuidv4(),
+  //     name: "Northern Ireland",
+  //     population: "1,885,000",
+  //     capital: "Belfast",
+  //   }
+  // ]
 
-  const handleNewCountry = (newCountry) => {
-    console.log(newCountry);
-    const currentCountries = ((countries) => [...countries, newCountry])
-    setCountries(currentCountries);
-  }
-  const handleDeleteCountry = (id) => {
-    const currentCountries = countries.filter((Country) => Country.id !== id);
-    setCountries(currentCountries);
-  }
+  // const [countries, setCountries] = useState(initialCountries)
+
+  // const handleNewCountry = (newCountry) => {
+  //   console.log(newCountry);
+  //   const currentCountries = ((countries) => [...countries, newCountry])
+  //   setCountries(currentCountries);
+  // }
+  // const handleDeleteCountry = (id) => {
+  //   const currentCountries = countries.filter((Country) => Country.id !== id);
+  //   setCountries(currentCountries);
+  // }
+
+  const {data, loading, error } = useFetch(url)
 
   return (
     <div className="App">
-      <Title />
+      {loading && <p>Loading...</p>}
+      {error ? <p>{error}</p>: <Todos todos={data} />}
+      {
+      /* <Title />
       <NewCountry onCreate={handleNewCountry}/>
-      <Countries countriesInfo={countries} onDelete={handleDeleteCountry}/>
+      <Countries countriesInfo={countries} onDelete={handleDeleteCountry}/> */}
     </div>
   );
 }
